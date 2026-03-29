6fbb4cd66af94a81ca753ad7be7e97eedff6baf1
Amit <codersgrow@gmail.com>
2026-03-29 11:15:56 +0530

    Setting up our Database

--- project-1/Backend/src/server.js
+++ project-1/Backend/src/server.js
@@ -1,7 +1,17 @@
 import express from 'express';
 import notesRoutes from './routes/notesRoutes.js';
+import { connect } from 'mongoose';
+import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+console.log(process.env.MONGO_URI)
 
 const app = express();
+const PORT = process.env.PORT || 5500;
+
+connectDB();
 
 app.use('/api/notes', notesRoutes);
 
@@ -10,3 +20,5 @@ app.listen(5500, () => {
 });
 
 // endPoint? - an endpoint is the combination of URL + HTTP method that lets the client iteract with a specific resources
+
+
